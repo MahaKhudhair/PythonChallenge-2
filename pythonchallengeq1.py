3d3b20e90a7fddebb26a02fbc6cfdf6a5f8df9fa
MahaKhudhair <41030892+MahaKhudhair@users.noreply.github.com>
2018-08-14 04:16:12 +0300

    Add files via upload

---
+++ pythonchallengeq1.py
@@ -0,0 +1,7 @@
+Python 3.7.0b4 (v3.7.0b4:eb96c37699, May  2 2018, 19:02:22) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> L1 = ['Iraq', 'Japan', 'Australia', 'Egypt', 'Turkey']
+>>> L2 = ['Sydney', 'Istanbul', 'Tokyo', 'Baghdad', 'Cairo']
+>>> print ([L1[0] , L2[3]] , [L1[1] , L2[2]] , [L1[2] ,L2[0]] , [L1[3] , L2[4]] , [L1[4] , L2[1]] )
+['Iraq', 'Baghdad'] ['Japan', 'Tokyo'] ['Australia', 'Sydney'] ['Egypt', 'Cairo'] ['Turkey', 'Istanbul']
+>>> 
